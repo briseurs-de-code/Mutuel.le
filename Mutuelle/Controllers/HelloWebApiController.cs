6e42ff45eb5ca8b4a78f395b8c7c467ade6db0d8
Unknown <sven.thommet@gmail.com>
2018-11-14 19:11:50 +0100

    ADD Emgu pour recherche d'images + tests Emgu

--- Mutuelle/Controllers/HelloWebApiController.cs
+++ Mutuelle/Controllers/HelloWebApiController.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Http.Results;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.IO;
 
 namespace Mutuelle.Controllers
 {
@@ -28,9 +31,18 @@ namespace Mutuelle.Controllers
         /// <returns>The get.</returns>
         public JsonResult<List<string>> Get()
         {
+            Mat pattern = new Mat(‪"C:\\Users\\svent\\Documents\\DotNet\\Mutuel.le\\Mutuelle\\Assets\\harmonie.png");
+            Mat carteTest = new Mat("/Assets/carte_test.png");
+            Mat result = null;
+
+            Emgu.CV.CvInvoke.MatchTemplate(carteTest, pattern, result, Emgu.CV.CvEnum.TemplateMatchingType.Ccoeff);
+
             List<String> collection = new List<String>
             {
-                "abc"
+               //Path.GetFullPath("HelloWebApiController.cs"),
+               //Path.GetFullPath("Assets/carte_test.png"),
+               //Path.GetFullPath("Mutuelle/Assets/carte_test.png"),
+               Path.GetPathRoot("/"),
             };
 
             return Json(collection);
