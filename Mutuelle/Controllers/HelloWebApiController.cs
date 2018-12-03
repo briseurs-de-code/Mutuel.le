c4923e1a548e0407e060b041284c28378a959557
Unknown <sven.thommet@gmail.com>
2018-12-03 10:01:49 +0100

    Test de EmguCV + correction des paths

--- Mutuelle/Controllers/HelloWebApiController.cs
+++ Mutuelle/Controllers/HelloWebApiController.cs
@@ -9,6 +9,7 @@ using System.Web.Http.Results;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using System.IO;
+using System.Web.Hosting;
 
 namespace Mutuelle.Controllers
 {
@@ -31,18 +32,22 @@ namespace Mutuelle.Controllers
         /// <returns>The get.</returns>
         public JsonResult<List<string>> Get()
         {
-            Mat pattern = new Mat(‪"C:\\Users\\svent\\Documents\\DotNet\\Mutuel.le\\Mutuelle\\Assets\\harmonie.png");
-            Mat carteTest = new Mat("/Assets/carte_test.png");
-            Mat result = null;
+            //var filename = @"C:\Users\svent\Documents\DotNet\Mutuel.le\Mutuelle\Assets\harmonie.png";
+            var carteFilename = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "Assets", "carte_test.png");
+            var patternFilename = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "Assets", "harmonie.png");
+
+            //if (!File.Exists(filename)) return null;
+            //if (!File.Exists(carteFilename)) return null;
+
+            Mat pattern = new Mat(patternFilename);
+            Mat carteTest = new Mat(carteFilename);
+            Mat result = new Mat();
 
             Emgu.CV.CvInvoke.MatchTemplate(carteTest, pattern, result, Emgu.CV.CvEnum.TemplateMatchingType.Ccoeff);
 
             List<String> collection = new List<String>
             {
-               //Path.GetFullPath("HelloWebApiController.cs"),
-               //Path.GetFullPath("Assets/carte_test.png"),
-               //Path.GetFullPath("Mutuelle/Assets/carte_test.png"),
-               Path.GetPathRoot("/"),
+               "test"
             };
 
             return Json(collection);
